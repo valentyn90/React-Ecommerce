705de31b14674f9e07c66d6f1832bf1ffe6687ed
webdev93 <web.dev903@gmail.com>
2019-07-26 03:50:04 +0700

    update navbar with box component

--- client-web/src/components/Navbar.js
+++ client-web/src/components/Navbar.js
@@ -2,12 +2,20 @@
  * @Author: Prawee Wongsa 
  * @Date: 2019-07-26 03:44:29 
  * @Last Modified by: Prawee Wongsa
- * @Last Modified time: 2019-07-26 03:45:15
+ * @Last Modified time: 2019-07-26 03:49:47
  */
 import React from 'react';
+import { Box } from 'gestalt';
 
 const Navbar = () => (
-  <div>Navbar</div>
+  <Box
+    height={70}
+    color="midnight"
+    padding={1}
+    shape="roundedBottom"
+  >
+    {/* Sign In Link */}
+  </Box>
 )
 
 export default Navbar;
