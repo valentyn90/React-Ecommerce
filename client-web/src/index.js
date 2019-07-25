2a71b3128ff40e01c13968a89ea9b24a8a32c5ef
webdev93 <web.dev903@gmail.com>
2019-07-26 03:36:01 +0700

    override root comp with signin, signup, checkout

--- client-web/src/index.js
+++ client-web/src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import './index.css';
 import App from './components/App';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import CheckOut from './components/CheckOut';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route component={App} exact path="/" />
+      <Route component={SignIn} path="/signin" />
+      <Route component={SignUp} path="/signup" />
+      <Route component={CheckOut} path="/checkout" />
+    </Switch>
+  </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
