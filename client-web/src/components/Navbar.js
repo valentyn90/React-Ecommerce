9bee999014a3a91db45ac070bc6a94d09c57c6df
webdev93 <web.dev903@gmail.com>
2019-07-26 03:54:00 +0700

    add navlink for signin and signup on navbar

--- client-web/src/components/Navbar.js
+++ client-web/src/components/Navbar.js
@@ -2,10 +2,11 @@
  * @Author: Prawee Wongsa 
  * @Date: 2019-07-26 03:44:29 
  * @Last Modified by: Prawee Wongsa
- * @Last Modified time: 2019-07-26 03:49:47
+ * @Last Modified time: 2019-07-26 03:53:28
  */
 import React from 'react';
-import { Box } from 'gestalt';
+import { Box, Text } from 'gestalt';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
   <Box
@@ -15,6 +16,18 @@ const Navbar = () => (
     shape="roundedBottom"
   >
     {/* Sign In Link */}
+    <NavLink to="/signin">
+      <Text size="x1" color="white">
+        Sign In
+      </Text>
+    </NavLink>
+
+    {/* Sign Up Link */}
+    <NavLink to="/signup">
+      <Text size="x1" color="white">
+        Sign Up
+      </Text>
+    </NavLink>
   </Box>
 )
 
