56ddd3d6de6e7c4ac3e624e81f532ee05405e205
webdev93 <web.dev903@gmail.com>
2019-07-26 03:45:30 +0700

    create navbar component

---
+++ client-web/src/components/Navbar.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Prawee Wongsa 
+ * @Date: 2019-07-26 03:44:29 
+ * @Last Modified by: Prawee Wongsa
+ * @Last Modified time: 2019-07-26 03:45:15
+ */
+import React from 'react';
+
+const Navbar = () => (
+  <div>Navbar</div>
+)
+
+export default Navbar;
