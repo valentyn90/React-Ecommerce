27565ccffc6ac20c92b8236f74c27dc6d62d3e6b
webdev93 <web.dev903@gmail.com>
2019-07-28 02:11:33 +0700

    fetch data from server to componentDidMount

--- client-web/src/components/App.js
+++ client-web/src/components/App.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Box, Heading } from 'gestalt';
 import './App.css';
+import Strapi from 'strapi-sdk-javascript/build/main';
 
-function App() {
-  return (
-    <Container>
-      {/* brands Section */}
-      <Box
-        display="flex"
-        justifyContent="center"
-        marginBottom={2}
-      >
-        {/* Brands Header */}
-        <Heading color="midnight" size="md">
-          Brew Brands
-        </Heading>
-      </Box>
-    </Container>
-  );
+const apiUrl = process.env.API_URL || 'http://localhost:1337';
+const strapi = new Strapi(apiUrl);
+
+class App extends Component {
+  async componentDidMount() {
+    try {
+      const response = await strapi.request('POST', '/graphql', {
+        data: {
+          query: `
+            query {
+              brands {
+                _id
+                name
+                description
+                image {
+                  url
+                }
+              }
+            } 
+          `
+        }
+      });
+      console.log(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        {/* brands Section */}
+        <Box
+          display="flex"
+          justifyContent="center"
+          marginBottom={2}
+        >
+          {/* Brands Header */}
+          <Heading color="midnight" size="md">
+            Brew Brands
+          </Heading>
+        </Box>
+      </Container>
+    );
+  }
 }
 
 export default App;
