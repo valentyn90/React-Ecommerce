abd21021d864e31d6e941b9e1e6133f9a62b7e7f
webdev93 <web.dev903@gmail.com>
2019-07-28 01:55:08 +0700

    update app contrainer

--- client-web/src/components/App.js
+++ client-web/src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Container, Box, Heading } from 'gestalt';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <Container>
+      {/* brands Section */}
+      <Box
+        display="flex"
+        justifyContent="center"
+        marginBottom={2}
+      >
+        {/* Brands Header */}
+        <Heading color="midnight" size="md">
+          Brew Brands
+        </Heading>
+      </Box>
+    </Container>
   );
 }
 
