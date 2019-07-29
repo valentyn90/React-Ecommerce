9231d606c0b7159dff3625e698846db4e5f0c7df
webdev93 <web.dev903@gmail.com>
2019-07-30 04:47:42 +0700

    install dotenv package

--- client-web/src/serviceWorker.js
+++ client-web/src/serviceWorker.js
@@ -10,6 +10,9 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
+require('dotenv').config();
+// console.log('env => ', process.env);
+
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
