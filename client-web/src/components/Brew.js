e0f1fc7eb717f70e8bf827c055af4452802e564a
webdev93 <web.dev903@gmail.com>
2019-07-29 14:32:00 +0700

    create brew component

---
+++ client-web/src/components/Brew.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: Prawee Wongsa 
+ * @Date: 2019-07-29 14:27:32 
+ * @Last Modified by: Prawee Wongsa
+ * @Last Modified time: 2019-07-29 14:28:45
+ */
+import React from 'react';
+
+class Brew extends React.Component {
+  render() {
+    return (
+      <div>Brew</div>
+    );
+  }
+}
+
+export default Brew;
