7b3a7056e4792e2c1a1e128290cd7b2dd3f64fcd
webdev93 <web.dev903@gmail.com>
2019-07-29 22:14:26 +0700

    add getCart,  setCart on localStorage

--- client-web/src/utils/index.js
+++ client-web/src/utils/index.js
@@ -2,11 +2,26 @@
  * @Author: Prawee Wongsa 
  * @Date: 2019-07-29 20:10:26 
  * @Last Modified by: Prawee Wongsa
- * @Last Modified time: 2019-07-29 20:13:05
+ * @Last Modified time: 2019-07-29 22:13:19
  */
+const CART_KEY = 'cart';
+ 
 export const calculatePrice = items => {
   return `$${items
     .reduce((acc, item) => acc + item.quantity * item.price, 0)
     .toFixed(2)
   }`;
+}
+
+export const setCart = (value, cartKey = CART_KEY) => {
+  if (localStorage) {
+    localStorage.setItem(cartKey, JSON.stringify(value));
+  }
+}
+
+export const getCart = (cartKey = CART_KEY) => {
+  if (localStorage && localStorage.getItem(cartKey)) {
+    return JSON.parse(localStorage.getItem(cartKey));
+  }
+  return [];
 }
