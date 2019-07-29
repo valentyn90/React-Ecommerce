903bf04f212c1b8375da0e6b74ac8c5846ee115e
webdev93 <web.dev903@gmail.com>
2019-07-30 02:15:43 +0700

    add getToken function

--- client-web/src/utils/index.js
+++ client-web/src/utils/index.js
@@ -2,7 +2,7 @@
  * @Author: Prawee Wongsa 
  * @Date: 2019-07-29 20:10:26 
  * @Last Modified by: Prawee Wongsa
- * @Last Modified time: 2019-07-29 23:25:25
+ * @Last Modified time: 2019-07-30 02:14:56
  */
 const CART_KEY = 'cart';
 const TOKEN_KEY = 'jwt';
@@ -32,4 +32,11 @@ export const setToken = (value, tokenKey = TOKEN_KEY) => {
   if (localStorage) {
     localStorage.setItem(tokenKey, JSON.stringify(value));
   }
+}
+
+export const getToken = (tokenKey = TOKEN_KEY) => {
+  if (localStorage && localStorage.getItem(tokenKey)) {
+    return JSON.parse(localStorage.getItem(tokenKey));
+  }
+  return null;
 }
