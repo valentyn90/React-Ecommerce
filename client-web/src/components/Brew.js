fb09f4b38c0d04422e484cb27099cc1513f1916b
webdev93 <web.dev903@gmail.com>
2019-07-29 17:14:28 +0700

    display brew

--- client-web/src/components/Brew.js
+++ client-web/src/components/Brew.js
@@ -2,7 +2,7 @@
  * @Author: Prawee Wongsa 
  * @Date: 2019-07-29 14:27:32 
  * @Last Modified by: Prawee Wongsa
- * @Last Modified time: 2019-07-29 15:21:30
+ * @Last Modified time: 2019-07-29 17:12:59
  */
 import React from 'react';
 import Strapi from 'strapi-sdk-javascript/build/main';
@@ -28,6 +28,7 @@ class Brew extends React.Component {
               brews {
                 _id
                 name
+                description
                 image {
                   url
                 }
@@ -49,8 +50,78 @@ class Brew extends React.Component {
     }
   }
   render() {
+    const { brand, brews } = this.state;
+
     return (
-      <div>Brew</div>
+      <Box
+        marginTop={4}
+        display="flex"
+        justifyContent="center"
+        alignItems="start"
+      >
+        {/* Brew Section */}
+        <Box display="flex" direction="column" alignItems="center">
+          {/* Brew Heading */}
+          <Box margin={2}>
+            <Heading color="orchid">{brand}</Heading>
+          </Box>
+          {/* Brews */}
+          <Box
+            dangerouslySetInlineStyle={{
+              __style: {
+                backgroundColor: '#bdcdd9'
+              }
+            }}
+            shape="rounded"
+            display="flex"
+            justifyContent="center"
+            padding={4}
+          >
+            {brews.map(brew => (
+              <Box
+              paddingY={4}
+              margin={2}
+              width={210}
+              key={brew._id}
+            >
+              <Card
+                image={
+                  <Box height={250} width={200}>
+                    <Image
+                      fit="cover"
+                      alt="Brand"
+                      naturalHeight={1}
+                      naturalWidth={1}
+                      src={`${apiUrl}${brew.image.url}`}
+                    />
+                  </Box>
+                }
+              >
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  direction="column"
+                >
+                  <Box margin={2}>
+                    <Text bold size="xl">
+                      {brew.name}
+                    </Text>
+                  </Box>
+                  <Text>{brew.description}</Text>
+                  <Text color="orchid">${brew.price}</Text>
+                  <Box marginTop={2}>
+                    <Text bold size="xl">
+                      <Button color="blue" text="Add to Cart" />
+                    </Text>
+                  </Box>
+                </Box>
+              </Card>
+            </Box>
+            ))}
+          </Box>
+        </Box>
+      </Box>
     );
   }
 }
